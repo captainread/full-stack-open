18e31c932617b2ee632fc9785ad67b729743b0b8
Alice Read <aread773@gmail.com>
2023-04-01 15:17:58 +0100

    1.3: course information step3

--- 1/part1/src/App.js
+++ 1/part1/src/App.js
@@ -1,17 +1,14 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = { title: "Fundamentals of React", ex: 10 };
+  const part2 = { title: "Using props to pass data", ex: 7 };
+  const part3 = { title: "State of a component", ex: 14 };
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   );
 };
@@ -20,12 +17,12 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = (content) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part title={content.part1} ex={content.exercises1} />
-      <Part title={content.part2} ex={content.exercises2} />
-      <Part title={content.part3} ex={content.exercises3} />
+      <Part title={part1.title} ex={part1.ex} />
+      <Part title={part2.title} ex={part2.ex} />
+      <Part title={part3.title} ex={part3.ex} />
     </div>
   );
 };
@@ -33,13 +30,15 @@ const Content = (content) => {
 const Part = (part) => {
   return (
     <div>
-      <p>{part.title} ({part.ex} exercises)</p>
+      <p>
+        {part.title} ({part.ex} exercises)
+      </p>
     </div>
-  )
-}
+  );
+};
 
-const Total = (props) => {
-  return <p>Total exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>;
+const Total = ({ part1, part2, part3 }) => {
+  return <p>Total exercises: {part1.ex + part2.ex + part3.ex}</p>;
 };
 
 export default App;
